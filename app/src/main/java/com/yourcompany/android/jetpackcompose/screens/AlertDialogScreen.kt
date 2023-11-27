aac05fc6e899b5cc5dd1aeeddc754adee5aaf337
guilherme <guilhermeestevo@gmail.com>
2023-11-27 20:49:33 -0300

    Progress indicators e dialogs

--- app/src/main/java/com/yourcompany/android/jetpackcompose/screens/AlertDialogScreen.kt
+++ app/src/main/java/com/yourcompany/android/jetpackcompose/screens/AlertDialogScreen.kt
@@ -34,7 +34,18 @@
 
 package com.yourcompany.android.jetpackcompose.screens
 
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.yourcompany.android.jetpackcompose.R
 import com.yourcompany.android.jetpackcompose.router.BackButtonHandler
 import com.yourcompany.android.jetpackcompose.router.JetFundamentalsRouter
 import com.yourcompany.android.jetpackcompose.router.Screen
@@ -52,5 +63,31 @@ fun AlertDialogScreen() {
 
 @Composable
 fun MyAlertDialog() {
-  //TODO add your code here
+  val shouldShowDialog = remember { mutableStateOf(true) }
+  if(shouldShowDialog.value) {
+    AlertDialog(
+      onDismissRequest = {
+        shouldShowDialog.value = false
+        JetFundamentalsRouter.navigateTo(Screen.Navigation) },
+      title = {
+        Text(text = stringResource(id = R.string.alert_dialog_title))
+      },
+      text = {
+        Text(text = stringResource(id = R.string.alert_dialog_text))
+      },
+      confirmButton = {
+        TextButton(
+          onClick = {
+            shouldShowDialog.value = false
+            JetFundamentalsRouter.navigateTo(Screen.Navigation)
+                    },
+        ) {
+          Text(
+            text = stringResource(id = R.string.confirm),
+            color = colorResource(id = R.color.colorPrimary),
+          )
+        }
+      }
+    )
+  }
 }
